a0e844b572448eef9ace8dbe17acd0998ac2534b
Tim Wyrzy <timothee.wyrzykowski@edu.ece.fr>
2025-04-20 23:00:47 +0200

    d

--- src/Vue/VueConnexion.java
+++ src/Vue/VueConnexion.java
@@ -6,8 +6,6 @@ import java.awt.event.ActionListener;
 import Modele.Utilisateur;
 
 import DAO.ChercherUtilisateur;
-import DAO.UtilisateurDAO;
-import DAO.RechercheDAO;
 
 public class VueConnexion extends JFrame {
     private JTextField emailField;
