cd3aff31a8cb7c06967e952d7a96b767b96b6fcd
Mldarius <131437136+Mldarius@users.noreply.github.com>
2025-04-24 14:41:48 +0200

    Update VuePanier.java

--- src/Vue/VuePanier.java
+++ src/Vue/VuePanier.java
@@ -140,7 +140,7 @@ public class VuePanier extends JFrame {
         searchButton.addActionListener(e -> {
             String texteRecherche = searchField.getText().trim();
             if(!texteRecherche.isEmpty()) {
-                new VueRecherche(texteRecherche);
+                new VueRecherche(texteRecherche, utilisateur);
                 dispose();
             }
         });
