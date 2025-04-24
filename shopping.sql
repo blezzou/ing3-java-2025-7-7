835e8c81bd9ae4fe81d59bc3cbd4251a5745965c
Tim Wyrzy <timothee.wyrzykowski@edu.ece.fr>
2025-04-24 21:38:04 +0200

    articles remis

--- shopping.sql
+++ shopping.sql
@@ -47,9 +47,37 @@ CREATE TABLE IF NOT EXISTS `article` (
 --
 
 INSERT INTO `article` (`id_article`, `nom`, `image`, `marque`, `description`, `prix`, `prix_vrac`, `quantite`, `quantite_vrac`, `note`) VALUES
-(1, 'article 1', '', 'marque 1', 'description 1', 10.00, 5.00, 10, 10, 0),
-(2, 'article 3', '', 'marque 2', 'description 2', 100.00, 50.00, 100, 100, 0),
-(5, 'test', 'test', 'test', 'test', 1.00, 1.00, 1, 1, 0);
+    (1, 'Jean', 'images_Article/jean_levis_511.jpg', 'Levi\'s', 'Le Levi\'s 511 offre une silhouette moderne avec sa coupe slim fit. En denim stretch pour une liberté de mouvement, ce pantalon allie élégance et confort. Parfait pour une tenue quotidienne !', 35.00, 26.00, 30, 15, 4),
+    (2, 'Pantalon de survêtement', 'images_Article/pantalon_adidas.jpg', 'Adidas', 'Pantalon de sport technique Adidas Tiro 23 en polyester stretch. Coupe tapered avec bandes contrastées, ceinture élastiquée et poches zippées. Insertions stretch aux genoux et traitement anti-odeurs. Idéal entraînement et style urbain.', 45.00, 33.00, 30, 20, 4),
+    (3, 'Chaussures', 'images_Article/chaussure_nike.jpg', 'Nike', 'Chaussures Nike Air Max 270 avec amorti Air Max visible pour un confort inégalé. Upper en mesh respirant, semelle en caoutchouc et logo swoosh contrasté. Idéales pour le sport et le style urbain. Disponibles en plusieurs coloris. Poids léger (320g).', 60.00, 46.00, 25, 13, 5),
+    (4, 'Chapeau de plage', 'images_Article/chapeau_tencoz.jpg', 'Tencoz', 'Chapeau de plage avec protection UV 50+, bord large 7cm en polyester respirant. Bandeau coton intérieur et lanière réglable. Léger (120g) et pliable. Tailles 54-60cm. Parfait pour plage, piscine et activités nautiques. ', 38.55, 19.99, 24, 15, 3),
+    (5, 'Tee-Shirt', 'images_Article/tshirt_boss.jpg', 'Boss', 'Offrez-vous ce tee-shirt de marque BOSS, synonyme de qualité et de sophistication. Conçu dans un matériau doux et respirant (coton ou mélange selon modèle), ce t-shirt allie confort et durabilité pour un port agréable au quotidien ! ', 20.00, 12.50, 40, 25, 3),
+    (6, 'Manteau', 'images_Article/manteau_tyqqu.jpg', 'Tyqqu', 'Mélange de coton et de polyester, fausse fourrure de haute qualité, doux, confortable et super chaud. Manteau d\'hiver classique et simple, couleur unie, vous rend plus belle, tendance et élégante.', 110.00, 85.00, 15, 10, 4),
+    (7, 'Maillot de bain pour femmes', 'images_Article/maillotdebainF.jpg', 'Nabaiji', 'Découvrez ce maillot de bain une pièce féminin, alliant style, confort et maintien pour des moments détente ou sportifs en toute élégance. Idéal pour la plage, la piscine ou les vacances !', 9.00, 6.00, 20, 12, 3),
+    (8, 'Maillot de bain pour hommes', 'images_Article/maillotdebainH.jpg', 'Haizid', 'Maillots de Bain Anti-Frottement pour Hommes HAZID - Séchage Rapide, Polyester Extensible & Spandex, Noir avec Lettrage Blanc, Taille Élastique, Lavable en Machine pour la Natation Compétitive & Récréative.', 7.00, 4.50, 30, 15, 3),
+    (9, 'Pull', 'images_Article/pull_jinkang.jpg', 'Jinkang', 'Pull en Tricot Ajusté pour Homme - Chaud, Extensible en Polyester avec Motif Géométrique pour Automne/Hiver', 19.50, 12.99, 35, 22, 3),
+    (10, 'Tee-Shirt', 'images_Article/tshirt_basic.jpg', 'Basic', 'Tee-shirt basique en coton 100% de coupe standard. Col rond et manches courtes. Matière douce et respirante pour un confort quotidien. Idéal pour toutes les occasions.', 12.90, 8.90, 50, 30, 4),
+    (11, 'Tee-Shirt', 'images_Article/tshirt_uniqlo.jpg', 'Uniqlo', 'Tee-shirt uni noir en coton stretch. Coupe légèrement ajustée, résistant au rétrécissement. Lavage facile, parfait pour un look minimaliste.', 14.90, 9.90, 45, 25, 4),
+    (12, 'Jean', 'images_Article/jean_levis_501.jpeg', 'Levi\'s', 'Jean classique 501 en denim pur coton. Coupe droite, boutons métalliques et cinq poches. Intemporel et durable pour un style décontracté.', 59.90, 45.00, 35, 20, 5),
+    (13, 'Jean', 'images_Article/jean_h&m.jpg', 'H&M', 'Jean slim en denim stretch avec élasthanne. Coupe ajustée mais confortable, ceinture à boucles et fermeture à bouton.', 39.90, 19.90, 40, 25, 3),
+    (14, 'Pull', 'images_Article/pull_zara.jpg', 'Zara', 'Pull unicolore en coton doux avec col rond. Coupe oversize pour un style décontracté. Manches longues et ourlet côtelé.', 29.90, 19.90, 30, 18, 4),
+    (15, 'Pull', 'images_Article/pull_mango.jpg', 'Mango', 'Pull en laine mérinos avec col en rond. Coupe ajustée, idéal pour superposer ou porter seul. Doux et chaud pour l\'hiver.', 29.90, 29.90, 25, 15, 4),
+    (16, 'Chemise', 'images_Article/chemise_boss.jpeg', 'Boss', 'Chemise classique en coton poplin. Col classique et coupe slim. Parfaite pour les occasions formelles.', 49.90, 35.00, 20, 12, 4),
+    (17, 'Chemise', 'images_Article/chemise_tommy.jpg', 'Tommy Hilfiger', 'Chemise en coton à rayures fines. Col boutonné et poches poitrine. Style décontracté mais soigné pour un look quotidien.', 44.90, 32.00, 22, 14, 4),
+    (18, 'Sweat-shirt', 'images_Article/sweat_nike.jpg', 'Nike', 'Sweat-shirt en coton avec capuche, poche kangourou et manches longues. Logo brodé sur la poitrine.', 45.00, 32.00, 28, 17, 4),
+    (19, 'Sweat-shirt', 'images_Article/sweat_adidas.jpg', 'Adidas', 'Sweat-shirt uni en coton doux. Col rond et manches longues. Logo imprimé discret. Coupe confortable pour un style sportif.', 39.90, 29.90, 30, 18, 4),
+    (20, 'Veste', 'images_Article/veste_tommy.jpg', 'Tommy Hilfiger', 'Veste en jean classique avec poches avant et boutons pression. Style intemporel pour toutes les saisons.', 49.90, 35.00, 18, 10, 4),
+    (21, 'Veste', 'images_Article/veste_columbia.jpg', 'Columbia', 'Veste d\'hiver matelassée avec capuche intégrée. Poches et fermeture à zip. Parfaite pour le froid.', 59.90, 45.00, 15, 9, 4),
+    (22, 'Chaussures', 'images_Article/chaussures_converse.jpg', 'Converse', 'Baskets montantes \'All-Star\' en toile avec semelle en caoutchouc. Style intemporel et polyvalent.', 55.00, 42.00, 25, 15, 5),
+    (23, 'Chaussures', 'images_Article/chaussures_vans.jpg', 'Vans', 'Baskets noires basses en toile renforcée. Lacets et logo signature.', 65.00, 49.00, 20, 12, 4),
+    (24, 'Short', 'images_Article/short_diesel.jpg', 'Diesel', 'Short bleu en denim. Taille normale. Style décontracté pour l\'été.', 39.90, 29.90, 25, 15, 3),
+    (25, 'Pantalon', 'images_Article/pantalon_celio.jpg', 'Celio', 'Conçu dans un tissu résistant et confortable (coton, stretch, ou autre matière selon modèle), ce pantalon assure une coupe moderne et une liberté de mouvement au quotidien.', 53.80, 41.00, 38, 23, 5),
+    (26, 'Bonnet', 'images_Article/bonnet_stetson.jpg', 'Stetson', 'Fabriqué avec des matériaux premium, ce couvre-chef allie durabilité, confort et un style unique pour les amateurs de mode roots ou les adeptes d''un look vintage raffiné.', 6.59, 4.67, 45, 38, 3),
+    (27, 'Casquette', 'images_Article/casquette_sweetPants.jpg', 'Sweet-Pants', 'Parfaite pour compléter vos tenues décontractées ou sportives, cette casquette allie look tendance et qualité pour un style affirmé au quotidien.', 10.00, 6.50, 28, 19, 4),
+    (28, 'Sandalettes', 'images_Article/sandalette_gabor.jpg', 'Gabor', 'Idéales pour les beaux jours, ces sandales féminines offrent une morphologie étudiée et des matières haut de gamme pour un look chic et décontracté, en ville comme en vacances.', 65.00, 48.90, 30, 20, 4),
+    (29, 'Lunettes de soleil', 'images_Article/lunettes_level.jpg', 'Level', 'Parfaites pour compléter vos tenues streetwear ou vos outfits décontractés, ces lunettes offrent une protection UV optimale tout en restant légères et résistantes.', 20.00, 13.00, 40, 32, 3),
+    (30, 'Echarpe', 'images_Article/echarpe_pimkie.JPG', 'Pimkie', 'Craquez pour cette écharpe Pimkie, un accessoire mode incontournable pour allier chaleur et élégance pendant la saison froide. Avec son tissu doux et enveloppant, cette écharpe vous offre un confort optimal tout en sublimant tous vos looks.', 8.99, 5.70, 15, 11, 3);
+COMMIT;
 
 -- --------------------------------------------------------
 
@@ -148,3 +176,5 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
